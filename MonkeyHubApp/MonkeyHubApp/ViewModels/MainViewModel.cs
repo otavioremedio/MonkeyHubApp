dfcbc546a8b7b3ce3cec87bf7c63376d227863f3
Otavio Remedio <otavioremedio@gmail.com>
2017-04-27 20:07:58 -0300

    fim da aula sobre command

--- MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
+++ MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -1,64 +1,107 @@
 ﻿using System.Threading.Tasks;
+using Xamarin.Forms;
 
 namespace MonkeyHubApp.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
-        private string _descricao;
-        public string Descricao {
-            get { return _descricao;}
-            set {
-                SetProperty(ref _descricao, value);
-                //_descricao = value;
-                //OnPropertyChanged(nameof(Descricao));
-                //OnPropertyChanged();
+        private string _searchTerm;
+        public string SearchTerm
+        {
+            get { return _searchTerm; }
+            set
+            {
+                if(SetProperty(ref _searchTerm, value))
+                    SearchCommand.ChangeCanExecute();
             }
         }
 
-        private string _nome;
-        public string Nome
-        {
-            get { return _nome; }
-            set {
-                SetProperty(ref _nome, value);
-                //_nome = value;
-                //OnPropertyChanged(nameof(Nome));
-                //OnPropertyChanged();
-            }
-        }
+        public Command SearchCommand { get; }
+        
+        //private string _descricao;
+        //public string Descricao
+        //{
+        //    get { return _descricao;}
+        //    set {
+        //        SetProperty(ref _descricao, value);
+        //        //_descricao = value;
+        //        //OnPropertyChanged(nameof(Descricao));
+        //        //OnPropertyChanged();
+        //    }
+        //}
 
-        private int _idade;
+        //private string _nome;
+        //public string Nome
+        //{
+        //    get { return _nome; }
+        //    set {
+        //        SetProperty(ref _nome, value);
+        //        //_nome = value;
+        //        //OnPropertyChanged(nameof(Nome));
+        //        //OnPropertyChanged();
+        //    }
+        //}
 
-        public int Idade
-        {
-            get { return _idade; }
-            set { SetProperty(ref _idade, value); }
-        }
+        //private int _idade;
+
+        //public int Idade
+        //{
+        //    get { return _idade; }
+        //    set { SetProperty(ref _idade, value); }
+        //}
 
 
         public MainViewModel()
         {
-            Descricao = "Olá mundo! Eu estou aqui!";
+            SearchCommand = new Command(ExecuteSearchCommand, CanExecuteSearchCommand);
+            //Descricao = "Olá mundo! Eu estou aqui!";
 
-            Task.Delay(3000).ContinueWith(async t => {
-                Descricao = "Meu texto mudou";
+            //Task.Delay(3000).ContinueWith(async t => {
+            //    Descricao = "Meu texto mudou";
 
-                for (int i = 0; i < 10; i++)
-                {
-                    await Task.Delay(1000);
-                    Descricao = $"Meu texto mudou{i}";
-                }
-                //if (PropertyChanged != null)
-                //{
-                //    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Descricao"));
-                //}    
+            //    for (int i = 0; i < 10; i++)
+            //    {
+            //        await Task.Delay(1000);
+            //        Descricao = $"Meu texto mudou{i}";
+            //    }
+            //    //if (PropertyChanged != null)
+            //    //{
+            //    //    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Descricao"));
+            //    //}    
 
-                //PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(Descricao)));
+            //    //PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(Descricao)));
 
 
-            });
+            //});
         }
 
-       
+        bool CanExecuteSearchCommand()
+        {
+            return string.IsNullOrWhiteSpace(SearchTerm) == false;
+        }
+
+        async void ExecuteSearchCommand()
+        {
+            await Task.Delay(2000);
+            bool resposta = await App.Current.MainPage.DisplayAlert("MonkeyHubApp", 
+                                                    $"Você pesquisou por '{SearchTerm}'?", 
+                                                    "Sim",
+                                                    "Não");
+
+            if (resposta)
+            {
+                //qq coisa
+                await App.Current.MainPage.DisplayAlert("MonkeyHubApp",
+                                                   "Obrigado!",
+                                                   "OK");
+            }
+            else
+            {
+                await App.Current.MainPage.DisplayAlert("MonkeyHubApp",
+                                                   "De nada!",
+                                                   "OK");
+            }
+            //Debug.WriteLine($"Clique {DateTime.Now}");
+        }
     }
 }
