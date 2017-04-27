73e7cd55cd62dea3b5339fbe334313ba47ad4d6c
Otavio Remedio <otavioremedio@gmail.com>
2017-04-27 18:25:08 -0300

    Add project files.

---
+++ MonkeyHubApp/MonkeyHubApp/MainPage.xaml.cs
@@ -0,0 +1,14 @@
+﻿using MonkeyHubApp.ViewModels;
+using Xamarin.Forms;
+
+namespace MonkeyHubApp
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+            BindingContext = new MainViewModel();
+        }
+    }
+}
---
+++ MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -0,0 +1,64 @@
+﻿using System.Threading.Tasks;
+
+namespace MonkeyHubApp.ViewModels
+{
+    public class MainViewModel : BaseViewModel
+    {
+        private string _descricao;
+        public string Descricao {
+            get { return _descricao;}
+            set {
+                SetProperty(ref _descricao, value);
+                //_descricao = value;
+                //OnPropertyChanged(nameof(Descricao));
+                //OnPropertyChanged();
+            }
+        }
+
+        private string _nome;
+        public string Nome
+        {
+            get { return _nome; }
+            set {
+                SetProperty(ref _nome, value);
+                //_nome = value;
+                //OnPropertyChanged(nameof(Nome));
+                //OnPropertyChanged();
+            }
+        }
+
+        private int _idade;
+
+        public int Idade
+        {
+            get { return _idade; }
+            set { SetProperty(ref _idade, value); }
+        }
+
+
+        public MainViewModel()
+        {
+            Descricao = "Olá mundo! Eu estou aqui!";
+
+            Task.Delay(3000).ContinueWith(async t => {
+                Descricao = "Meu texto mudou";
+
+                for (int i = 0; i < 10; i++)
+                {
+                    await Task.Delay(1000);
+                    Descricao = $"Meu texto mudou{i}";
+                }
+                //if (PropertyChanged != null)
+                //{
+                //    PropertyChanged.Invoke(this, new PropertyChangedEventArgs("Descricao"));
+                //}    
+
+                //PropertyChanged.Invoke(this, new PropertyChangedEventArgs(nameof(Descricao)));
+
+
+            });
+        }
+
+       
+    }
+}
