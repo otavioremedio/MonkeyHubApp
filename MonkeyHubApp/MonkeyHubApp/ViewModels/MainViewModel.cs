326c88715cc64c1e557ac4f1b6220b0fc315dffa
Otavio Remedio <otavioremedio@gmail.com>
2017-04-28 19:10:17 -0300

    fim da aula sobre servicos

--- MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
+++ MonkeyHubApp/MonkeyHubApp/ViewModels/MainViewModel.cs
@@ -2,11 +2,39 @@
 using Xamarin.Forms;
 using System.Collections.ObjectModel;
 using System.Collections.Generic;
+using System.IO;
+using System.Net.Http;
+using System.Net.Http.Headers;
+using Newtonsoft.Json;
+using MonkeyHubApp.Models;
 
 namespace MonkeyHubApp.ViewModels
 {
     public class MainViewModel : BaseViewModel
     {
+        private const string BaseUrl = "https://monkey-hub-api.azurewebsites.net/api/";
+
+        public async Task<List<Tag>> GetTagsAsync()
+        {
+            var httpClient = new HttpClient();
+            httpClient.DefaultRequestHeaders.Accept.Add(new MediaTypeWithQualityHeaderValue("application/json"));
+
+            var response = await httpClient.GetAsync($"{BaseUrl}Tags").ConfigureAwait(false);
+
+            if (response.IsSuccessStatusCode)
+            {
+                using (var responseStream = await response.Content.ReadAsStreamAsync().ConfigureAwait(false))
+                {
+                    return JsonConvert.DeserializeObject<List<Tag>>(
+                        await new StreamReader(responseStream)
+                            .ReadToEndAsync().ConfigureAwait(false));
+                }
+            }
+
+            return null;
+        }
+
+
         private string _searchTerm;
         public string SearchTerm
         {
@@ -18,7 +46,7 @@ namespace MonkeyHubApp.ViewModels
             }
         }
         
-        public ObservableCollection<string> Resultados { get; }
+        public ObservableCollection<Tag> Resultados { get; }
 
         public Command SearchCommand { get; }
         
@@ -58,7 +86,7 @@ namespace MonkeyHubApp.ViewModels
         public MainViewModel()
         {
             SearchCommand = new Command(ExecuteSearchCommand, CanExecuteSearchCommand);
-            Resultados = new ObservableCollection<string>();
+            Resultados = new ObservableCollection<Tag>();
 
             //Descricao = "Olá mundo! Eu estou aqui!";
 
@@ -88,6 +116,7 @@ namespace MonkeyHubApp.ViewModels
 
         async void ExecuteSearchCommand()
         {
+            
             //await Task.Delay(2000);
             bool resposta = await App.Current.MainPage.DisplayAlert("MonkeyHubApp", 
                                                     $"Você pesquisou por '{SearchTerm}'?", 
@@ -97,24 +126,33 @@ namespace MonkeyHubApp.ViewModels
             if (resposta)
             {
                 //qq coisa
-                await App.Current.MainPage.DisplayAlert("MonkeyHubApp",
-                                                   "Obrigado!",
-                                                   "OK");
-                for (int i = 1; i < 15; i++)
+                //await App.Current.MainPage.DisplayAlert("MonkeyHubApp",
+                //                                    "Obrigado!",
+                //                                    "OK");
+
+                var tagsRetornadasDoServico = await GetTagsAsync();
+
+                Resultados.Clear();
+
+                if (tagsRetornadasDoServico != null)
                 {
-                    Resultados.Add($"Sim {i}");
+                    foreach (var tag in tagsRetornadasDoServico)
+                    {
+                        Resultados.Add(tag);
+                    }
                 }
-
-                
-
+                //for (int i = 1; i < 15; i++)
+                //{
+                //    Resultados.Add($"Sim {i}");
+                //}
             }
             else
             {
                 await App.Current.MainPage.DisplayAlert("MonkeyHubApp",
-                                                   "De nada!",
-                                                   "OK");
+                                                    "De nada!",
+                                                    "OK");
             }
-            //Debug.WriteLine($"Clique {DateTime.Now}");
-        }
+        //Debug.WriteLine($"Clique {DateTime.Now}");
+}
     }
 }
